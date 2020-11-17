a15bd5907ea01c4b2abf9f0aa1b9f1923d2a1dd8
NickVeale <novingle@gmail.com>
2020-11-18 10:27:16 +1300

    Made slight styling tweaks and committed for Git Practice

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,47 +1,92 @@
 import React from 'react';
 import './Profile.css';
 import ProfilePicture from '../../img/profilePicture.png';
+import teacherProfilePicture from '../../img/teacherProfilePicture.png';
 
 export default function Profile() {
-    return (
-        <div className="profileDiv">
-            <span className="profileContent">
-                <div className="profilePhoto">
-                    <img src={ProfilePicture} className="photo" alt=""></img>
-                    <button className="profileEditButton">EDIT PROFILE</button>
-                    <button className="profileEditButton">CHANGE PHOTO</button>
-                </div>
-                <div className="profileInfo">
-                    <h2 id="profileNameHeading">Rawiri Fletcher</h2>
-                    <div className="infoSpanContainer">
-                        <span className="infoSpan">
-                            <p>School</p>
-                            <p>Homai School</p>
-                        </span>
-                        <span className="infoSpan">
-                            <p>Teacher</p>
-                            <p>Jasmina Salvador</p>
-                        </span>
-                        <span className="infoSpan">
-                            <p>Course</p>
-                            <p>Beginner</p>
-                        </span>
-                        <span className="infoSpan">
-                            <p>Date of Birth</p>
-                            <p>25 June 2010</p>
-                        </span>
-                        <span className="infoSpan">
-                            <p>Contact No.</p>
-                            <p>022 524 63 99</p>
-                        </span>
-                        <span className="infoSpan">
-                            <p>Email Address</p>
-                            <p>fletchy.r@gmail.com</p>
-                        </span>
+
+    const [teacher, setTeacher] = React.useState(true);
+
+    if (teacher) {
+        return (
+            <div className="profileDiv">
+                <span className="profileContent">
+                    <div className="profilePhoto">
+                        <img src={teacherProfilePicture} className="photo" alt=""></img>
+                        <button className="profileEditButton">EDIT PROFILE</button>
+                        <button className="profileEditButton">CHANGE PHOTO</button>
+                        <button className="profileEditButton">SETTINGS</button>
                     </div>
-                </div>
-            </span>
-            <button className="backButton">BACK TO PROJECTS</button>
-        </div>
-    );
+                    <div className="profileInfo">
+                        <h2 id="profileNameHeading">Jasmina Salvador</h2>
+                        <div className="infoSpanContainer">
+                            <span className="infoSpan">
+                                <p>School</p>
+                                <p>Homai School</p>
+                            </span>
+                            <span className="infoSpan">
+                                <p>Courses Purchased</p>
+                                <p>Beginner</p>
+                            </span>
+                            <span className="infoSpan">
+                                <p>Date of Birth</p>
+                                <p>25 June 1986</p>
+                            </span>
+                            <span className="infoSpan">
+                                <p>Contact No.</p>
+                                <p>027 754 28 00</p>
+                            </span>
+                            <span className="infoSpan">
+                                <p>Email Address</p>
+                                <p>jsalvador@homai.edu</p>
+                            </span>
+                        </div>
+                    </div>
+                </span>
+                <button className="backProjButton">BACK TO PROJECTS</button><button className="backDashButton">BACK TO DASHBOARD</button>
+            </div>
+        );
+    } else {
+        return (
+            <div className="profileDiv">
+                <span className="profileContent">
+                    <div className="profilePhoto">
+                        <img src={ProfilePicture} className="photo" alt=""></img>
+                        <button className="profileEditButton">EDIT PROFILE</button>
+                        <button className="profileEditButton">CHANGE PHOTO</button>
+                    </div>
+                    <div className="profileInfo">
+                        <h2 id="profileNameHeading">Rawiri Fletcher</h2>
+                        <div className="infoSpanContainer">
+                            <span className="infoSpan">
+                                <p>School</p>
+                                <p>Homai School</p>
+                            </span>
+                            <span className="infoSpan">
+                                <p>Teacher</p>
+                                <p>Jasmina Salvador</p>
+                            </span>
+                            <span className="infoSpan">
+                                <p>Course</p>
+                                <p>Beginner</p>
+                            </span>
+                            <span className="infoSpan">
+                                <p>Date of Birth</p>
+                                <p>25 June 2010</p>
+                            </span>
+                            <span className="infoSpan">
+                                <p>Contact No.</p>
+                                <p>022 524 63 99</p>
+                            </span>
+                            <span className="infoSpan">
+                                <p>Email Address</p>
+                                <p>fletchy.r@gmail.com</p>
+                            </span>
+                        </div>
+                    </div>
+                </span>
+                <button className="backButton">BACK TO PROJECTS</button>
+            </div>
+        );
+    };
 };
