2e70897b26e32ee0ca7fa702cbee8ea2d69a9cfa
Juan <misa.jpb@gmail.com>
2020-11-18 12:54:57 +1300

    Juan made a folder

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Nav from './Components/Nav/Nav';
 import Projects from './Components/Projects/Projects';
 import Teachers from './Components/Teachers/Teachers';
 import Profile from './Components/Profile/Profile.js';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
@@ -19,19 +19,19 @@ const theme = createMuiTheme({
 });
 
 
-class App extends React.Component{
+class App extends React.Component {
   render() {
     return (
       <Router>
         <div>
-            <Nav />
-              <Switch>
-                <Route exact path="/" component={Homepage} />
-                <Route path="/projects" component={Projects} />
-                <Route path="/teachers" component={Teachers} />
-                <Route path="/profile" component={Profile} />
-              </Switch>
-        </div>  
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/teachers" component={Teachers} />
+            <Route path="/profile" component={Profile} />
+          </Switch>
+        </div>
       </Router>
     )
   }
