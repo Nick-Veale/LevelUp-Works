67c10609a732c518187119f554b9df85f20535ef
Justin Hume <justin.hume@live.com>
2020-11-20 15:13:13 +1300

    Homepage/Keycomp section

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import SignUp from './HomepageComponents/SignUp/SignUp';
-import Offer from './HomepageComponents/Offer/Offer';
-import Skills from './HomepageComponents/Skills/Skills';
+import React from "react";
+import SignUp from "./HomepageComponents/SignUp/SignUp";
+import Offer from "./HomepageComponents/Offer/Offer";
+import Skills from "./HomepageComponents/Skills/Skills";
+import KeyComp from "./HomepageComponents/KeyComp/KeyComp";
 
 export default function Homepage() {
-
-        return ( 
-            <div>
-                <SignUp />
-                <Offer />
-                <Skills />  
-            </div>
-         );
-
-};
+  return (
+    <div>
+      <SignUp />
+      <Offer />
+      <Skills />
+      <KeyComp />
+    </div>
+  );
+}
