a094a2b728e87a8e4eb6768befc4d73e93dc0a39
Justin Hume <justin.hume@live.com>
2020-12-17 20:45:19 +1300

    poo

--- src/Components/Homepage/HomepageComponents/Waiting/Waiting.js
+++ src/Components/Homepage/HomepageComponents/Waiting/Waiting.js
@@ -1,36 +1,49 @@
-import teacherPicture from '../../../../img/teacherPicture.png';
-import React, {useState} from 'react';
-import './Waiting.css';
-import NavModal from '../../../Nav/NavModal';
+import teacherPicture from "../../../../img/teacherPicture.png";
+import React, { useState } from "react";
+import "./Waiting.css";
+import NavModal from "../../../Nav/NavModal";
 
 export default function Waiting() {
+  const [open, setOpen] = useState(false);
 
-    const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleOpen = () => {
-        setOpen(true);
-    };
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    return (
-        <div className="waitingMainDiv">
-            <div className="waitingLeftDiv">
-                <img className="waitingLeftDivImage" src={teacherPicture} alt="" />
-            </div>
-            <div className="waitingRightDiv">
-                <div className="waitingTextDiv">
-                    <h2>What are you waiting for?</h2>
-                    <h3>Start teaching Digital Technologies today.</h3>
-                    <p>If you need more information, we are happy to answer any questions you may have.</p>
-                </div>
-                <div className="waitingButtonDiv">
-                    <button className="waitingEnquireButton">ENQUIRE NOW</button>
-                    <button onClick={() => handleOpen()} className="waitingSignUpButton">SIGN UP</button>
-                    <NavModal open={open} handleClose={handleClose} />
-                </div>
-            </div>
+  return (
+    <div className="waitingMainContainer">
+      <div className="waitingMainDiv">
+        <div className="containerBox">
+          <div className="waitingLeftDiv">
+            <img className="waitingLeftDivImage" src={teacherPicture} alt="" />
+          </div>
         </div>
-    )
-};
+        <div className="containerBox">
+          <div className="waitingRightDiv">
+            <div className="waitingTextDiv">
+              <h2>What are you waiting for?</h2>
+              <h3>Start teaching Digital Technologies today.</h3>
+              <p>
+                If you need more information, we are happy to answer any
+                questions you may have.
+              </p>
+            </div>
+            <div className="waitingButtonDiv">
+              <button className="waitingEnquireButton">ENQUIRE NOW</button>
+              <button
+                onClick={() => handleOpen()}
+                className="waitingSignUpButton"
+              >
+                SIGN UP
+              </button>
+              <NavModal open={open} handleClose={handleClose} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Components/Teachers/TeacherBuildWindow.js
+++ src/Components/Teachers/TeacherBuildWindow.js
@@ -1,70 +1,43 @@
-import React from 'react';
-import './TeacherDashboard.css';
-import { UserContext } from '../../userContext';
-import {Redirect} from 'react-router-dom';
+import React from "react";
+import "./TeacherDashboard.css";
+import HelpReq from "./HelpReq/HelpReq.js";
+import ProgTrack from "./ProgTrack/ProgTrack.js";
+import StudProf from "./StudProf/StudProf.js";
+import { UserContext } from "../../userContext";
+import { Redirect } from "react-router-dom";
 
 export default function ProjectBuildWindow(props) {
+  const { user, userContext } = React.useContext(UserContext);
 
-    const { user, userContext } = React.useContext(UserContext);
+  const handleRender = () => {
+    //prog track
+    if (props.contentId === 0) {
+      return <ProgTrack />;
+      //stud prof
+    } else if (props.contentId === 1) {
+      return <div className="teacherContentDiv"></div>;
+      //help req
+    } else if (props.contentId === 2) {
+      let x = 80;
 
-    const handleRender = () => {
-        if (props.contentId === 0) {
-            return (
-                <div className="teacherContentDiv">
-                   
-                </div>
-            )
-        } else if (props.contentId === 1) {
-            return (
-                <div className="teacherContentDiv">
-                  
-                </div>
-            )
-        } else if (props.contentId === 2) {
+      return <div className="teacherContextDiv"></div>;
+      //proj sub
+    } else if (props.contentId === 3) {
+      let x = 88;
 
-            let x = 80;
+      return <div className="teacherContentDiv"></div>;
+    } else if (props.contentId === 4) {
+      return <Redirect to="/projects" />;
+    }
+  };
 
-            return (
-                <div className="teacherContextDiv">
-                </div>
-            )
-        } else if (props.contentId === 3) {
-
-            let x = 88;
-
-            return (
-                <div className="teacherContentDiv">
-                   
-                </div>
-            )
-        } else if (props.contentId === 4) {
-                return (
-                    <Redirect to="/projects" />
-                )
-        } else if (props.contentId === 5) {
-            return (
-                <div className="teacherContentDiv">
-                </div>
-            )
-        } else if (props.contentId === 6) {
-            return (
-                <div className="teacherContentDiv">
-                </div>
-            )
-        }
-    } 
-
-    if (!user) {
-        return (
-            <Redirect to="/" />
-        );
-    } else {
-        return (
-            <div style={{height: '90vh'}}>
-                <div className="projectBuildWindowCurlyBox">
-                    {handleRender()}
-                </div>
-            </div>
-        );
-    };
-};
+  if (!user) {
+    return <Redirect to="/" />;
+  } else {
+    return (
+      <div style={{ height: "90vh" }}>
+        <div className="projectBuildWindowCurlyBox">{handleRender()}</div>
+      </div>
+    );
+  }
+}
